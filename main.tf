c6bc98a60932e4c0cdb48d44546c0b1f385464bf
Leonardo Reinoso <lreinoso@deloitte.com>
2020-04-16 17:08:25 -0300

    fisrt

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Configure the Azure provider
+provider "azurerm" {
+    version = "~>1.32.0"
+}
+
+# Create a new resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "myTFResourceGroup"
+    location = "eastus"
+
+    tags = {
+        Environment = "terraform getting started"
+        Team = "Devops"
+    }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+
